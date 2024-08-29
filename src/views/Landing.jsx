096403078c86842b455a50b3026f3b05bb1c1a8f
ivaniraldi <iraldiban@gmail.com>
2024-08-29 01:02:12 -0300

    Actualice la landing page

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Landing() {
   return (
-    <div>
+    <div className="landingPage">
         <h1>Bienvenido a <span className="text-success">UsuariosDB</span></h1>
         <Link to={"/home"}><button className="btn btn-primary">Entrar</button></Link>
     </div>
